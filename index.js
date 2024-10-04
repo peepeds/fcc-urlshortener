5846e117bff0d5cd178780cdc5b87170a177485a
peepeds <edbertmac@gmail.com>
2024-10-04 16:05:25 +0700

    chore : redirect wrong route

--- index.js
+++ index.js
@@ -6,20 +6,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 const router = require('./Router/urlRoute')
 
-
-
 require('dotenv').config();
 
 const connectDB = require('./models/connection');
 const bodyParser = require('body-parser');
 connectDB();
 
-
 // Konfigurasi dasar
 
 // Menggunakan CORS
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -27,25 +24,31 @@ app.use(bodyParser.json());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Route untuk mengakses index.html
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   const filePath = path.join(__dirname, 'views', 'index.html'); // Menggunakan path.join untuk jalur yang aman
   console.log('Trying to send file:', filePath); // Menampilkan jalur file di konsol
-  res.sendFile(filePath, function(err) {
+  res.sendFile(filePath, function (err) {
     if (err) {
       console.error('Error sending file:', err); // Menangani error jika ada
       res.status(err.status).send('Error loading the page.');
     }
   });
 });
+
+// Route untuk ping
 app.get('/ping', async (req, res) => {
   res.send(req.body);
 });
 
 // Menambahkan route baru
-
 app.use('/api', router);
 
+// Middleware untuk menangani 404 (route tidak ditemukan)
+app.use((req, res, next) => {
+  res.status(404).redirect('/');
+});
+
 // Menjalankan server
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
