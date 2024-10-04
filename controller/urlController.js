e8ba1f162418e04133103c1904a6c9fdf29865fb
peepeds <edbertmac@gmail.com>
2024-10-04 15:52:04 +0700

    fix: enhance algo

--- controller/urlController.js
+++ controller/urlController.js
@@ -2,7 +2,6 @@ const dns = require('dns');
 const crypto = require('crypto');
 const { urlModel } = require('../models/url');
 
-// Fungsi untuk menghasilkan short URL
 const generateShortURL = async (length) => {
     return crypto.randomBytes(length).toString('base64').replace(/\W/g, '').slice(0, length);
 }
@@ -11,37 +10,30 @@ const addUrl = async (req, res) => {
     try {
         const original_url = req.body.url;
 
-        // Validasi URL menggunakan objek URL dan pastikan hanya menerima skema "https://"
         let url;
         try {
             url = new URL(original_url);
             if (url.protocol !== 'https:') {
-                return res.json({ error: 'invalid url, only https is accepted' });
+                return res.json({ error: 'invalid url' });
             }
         } catch (error) {
-            return res.json({ error: 'invalid url format' });
+            return res.json({ error: 'invalid url' });
         }
 
-        // Ambil hostname untuk pengecekan DNS
         const host = url.hostname;
 
-        // Gunakan dns.lookup untuk memverifikasi host
         dns.lookup(host, async (err) => {
             if (err) {
-                return res.json({ error: 'invalid url, DNS lookup failed' });
+                return res.json({ error: 'invalid url' });
             }
-
-            // Hitung jumlah dokumen yang sudah ada dan gunakan sebagai short_url
             let short_url = await generateShortURL(5);
 
-            // Loop untuk memastikan tidak ada konflik dengan short_url yang sudah ada
             let checkShort = await urlModel.findOne({ short_url });
             while (checkShort) {
                 short_url = await generateShortURL(6);
                 checkShort = await urlModel.findOne({ short_url });
             }
 
-            // Simpan URL yang baru di database
             const newShortUrl = new urlModel({
                 original_url,
                 short_url
